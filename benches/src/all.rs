93d3b49cb614f7a952111cefe11f555ed5b2c1f2
Juan Aguilar Santillana <mhpoin@gmail.com>
2018-12-09 23:02:08 +0100

    add more tiny cases at benches

--- benches/src/all.rs
+++ benches/src/all.rs
@@ -1,3 +1,4 @@
+#![allow(unused_must_use)]
 extern crate v_htmlescape;
 #[macro_use]
 extern crate criterion;
@@ -11,48 +12,34 @@ use std::str;
 static HUGE: &[u8] = include_bytes!("../data/sherlock-holmes-huge.txt");
 // escapeable characters replaced by 'a'
 static HUGE_ED: &[u8] = include_bytes!("../data/sherlock-holmes-escaped-huge.txt");
+
 static SMALL: &[u8] = include_bytes!("../data/sherlock-holmes-small.txt");
 // escapeable characters replaced by 'a'
 static SMALL_ED: &[u8] = include_bytes!("../data/sherlock-holmes-escaped-small.txt");
+
 static TINY: &[u8] = include_bytes!("../data/sherlock-holmes-tiny.txt");
 // same size
 static TINY_ED: &[u8] = include_bytes!("../data/sherlock-holmes-escaped-tiny.txt");
+
+static VERY_TINY: &[u8] = b"ab>cdefghijklmnopqrstuvw<xyz";
+// escapeable characters replaced by '.'
+static VERY_TINY_ED: &[u8] = b"ab.cdefghijklmnopqrstuvw.xyz";
+
+static VV_TINY: &[u8] = b"abcd<efghijklm";
+// escapeable characters replaced by '.'
+static VV_TINY_ED: &[u8] = b"abcd.efghijklm";
+
+// Avx 11 characters limit performance
+static ULTRA_TINY: &[u8] = b"abcd<efghij";
+// escapeable characters replaced by '.'
+static ULTRA_TINY_ED: &[u8] = b"abcd.efghij";
+
+// Bad cases
+static ULTRA_V_TINY: &[u8] = b"abcd<ef";
+// escapeable characters replaced by '.'
+static ULTRA_V_TINY_ED: &[u8] = b"abcd.ef";
+
 static EMPTY: &[u8] = &[];
-
-fn functions(c: &mut Criterion) {
-
-    let group = "Sizing";
-    define(c, group, "huge", HUGE, sized(HUGE));
-    define(c, group, "huge escaped", HUGE_ED, sized(HUGE_ED));
-    define(c, group, "small", SMALL, sized(SMALL));
-    define(c, group, "small escaped", SMALL_ED, sized(SMALL_ED));
-    define(c, group, "tiny", TINY, sized(TINY));
-    define(c, group, "tiny escaped", TINY_ED, sized(TINY_ED));
-    define(c, group, "empty", EMPTY, sized(EMPTY));
-
-    let group = "Escaping";
-    define(c, group, "huge", HUGE, escaping(HUGE));
-    define(c, group, "huge escaped", HUGE_ED, escaping(HUGE_ED));
-    define(c, group, "small", SMALL, escaping(SMALL));
-    define(c, group, "small escaped", SMALL_ED, escaping(SMALL_ED));
-    define(c, group, "tiny", TINY, escaping(TINY));
-    define(c, group, "tiny escaped", TINY_ED, escaping(TINY_ED));
-    define(c, group, "empty", EMPTY, escaping(EMPTY));
-
-    let group = "Sized Escaping";
-    define(c, group, "huge", HUGE, size_escaping(HUGE));
-    define(c, group, "huge escaped", HUGE_ED, size_escaping(HUGE_ED));
-    define(c, group, "small", SMALL, size_escaping(SMALL));
-    define(c, group, "small escaped", SMALL_ED, size_escaping(SMALL_ED));
-    define(c, group, "tiny", TINY, size_escaping(TINY));
-    define(c, group, "tiny escaped", TINY_ED, size_escaping(TINY_ED));
-    define(c, group, "empty", EMPTY, size_escaping(EMPTY));
-
-    let group = "std Writing";
-    define(c, group, "huge", HUGE, writing(HUGE));
-    define(c, group, "small", SMALL, writing(SMALL));
-    define(c, group, "tiny", TINY, writing(TINY));
-}
 
 fn sized(corpus: &'static [u8]) -> impl FnMut(&mut Bencher) + 'static {
     move |b: &mut Bencher| {
@@ -106,6 +93,48 @@ fn define(
     let tput = Throughput::Bytes(corpus.len() as u32);
     let benchmark = Benchmark::new(bench_name, bench).throughput(tput);
     c.bench(group_name, benchmark);
+}
+
+#[rustfmt::skip]
+macro_rules! groups {
+    ($c:ident, $group:ident, $fun:ident) => {{
+        define($c, $group, "huge", HUGE, $fun(HUGE));
+        define($c, $group, "huge escaped", HUGE_ED, $fun(HUGE_ED));
+
+        define($c, $group, "small", SMALL, $fun(SMALL));
+        define($c, $group, "small escaped", SMALL_ED, $fun(SMALL_ED));
+
+        define($c, $group, "tiny", TINY, $fun(TINY));
+        define($c, $group, "tiny escaped", TINY_ED, $fun(TINY_ED));
+
+        define($c, $group, "very tiny", VERY_TINY, $fun(VERY_TINY));
+        define($c, $group, "very tiny escaped", VERY_TINY_ED, $fun(VERY_TINY_ED));
+
+        define($c, $group, "very very tiny", VV_TINY, $fun(VV_TINY));
+        define($c, $group, "very very tiny escaped", VV_TINY_ED, $fun(VV_TINY_ED));
+
+        define($c, $group, "ultra tiny", ULTRA_TINY, $fun(ULTRA_TINY));
+        define($c, $group, "ultra tiny escaped", ULTRA_TINY_ED, $fun(ULTRA_TINY_ED));
+
+        define($c, $group, "ultra very tiny", ULTRA_V_TINY, $fun(ULTRA_V_TINY));
+        define($c, $group, "ultra very tiny escaped", ULTRA_V_TINY_ED, $fun(ULTRA_V_TINY_ED));
+
+        define($c, $group, "empty", EMPTY, $fun(EMPTY));
+    }};
+}
+
+fn functions(c: &mut Criterion) {
+    let group = "Sizing";
+    groups!(c, group, sized);
+
+    let group = "Escaping";
+    groups!(c, group, escaping);
+
+    let group = "Sized Escaping";
+    groups!(c, group, size_escaping);
+
+    let group = "std Writing";
+    groups!(c, group, writing);
 }
 
 criterion_main!(benches);
