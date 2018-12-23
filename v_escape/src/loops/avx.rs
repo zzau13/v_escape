5b4a42a1e397adb4401c373a34e327f8dec4105f
Tomas Izquierdo Garcia-Faria <maklins08@gmail.com>
2018-12-23 02:07:20 +0100

    Documentation details avx

--- v_escape/src/loops/avx.rs
+++ v_escape/src/loops/avx.rs
@@ -1,11 +1,14 @@
-/// Main loop searches in byte slice with bit mask
+#[macro_export]
+/// Macro `loop_m256_128` is the main loop that searches in byte slice with a bit mask
+/// using adcanced vector extension (avx) optimizations.
 ///
-/// ## The following macros must be defined:
-/// * `write_mask(mask: {integer}, ptr: *const u8)` do operation at full mask
+/// ## The following macros must be defined
 ///
-/// * `write_forward(mask: {integer}, until: usize})` do operation at sliced mask
+/// * `write_mask(mask: {integer}, ptr: *const u8)` writes operation of full mask.
 ///
-/// * `masking(a: __m256i) -> __m256i` make a mask from __m256i
+/// * `write_forward(mask: {integer}, until: usize})` writes operation of sliced mask
+///
+/// * `masking(a: __m256i) -> __m256i` creates a mask from __m256i
 ///
 /// ## Example
 ///
@@ -59,8 +62,6 @@
 /// assert_eq!(unsafe { memchr(b'a', b"ba") }, Some(1));
 /// # }
 /// ```
-// TODO: document in detail
-#[macro_export]
 macro_rules! loop_m256_128 {
     ($len:ident, $ptr:ident, $start_ptr:ident, $bytes:ident) => {{
         #[allow(unused_imports)]
@@ -72,6 +73,8 @@ macro_rules! loop_m256_128 {
         const VECTOR_ALIGN: usize = VECTOR_SIZE - 1;
         const LOOP_SIZE: usize = 4 * VECTOR_SIZE;
 
+        // If the string length is less than VECTOR_SIZE
+        // unaligned function _mm256_loadu_si256 is used
         if $len < VECTOR_SIZE {
             #[allow(unused_mut)]
             let mut mask = {
@@ -82,9 +85,14 @@ macro_rules! loop_m256_128 {
             if mask != 0 {
                 write_forward!(mask, $len);
             }
+        // If string length is larger than VECTOR_SIZE,
+        // then it can be sliced into more than one set
+        // of VECTOR_SIZE elements and processed
         } else {
             let end_ptr = $bytes[$len..].as_ptr();
 
+            // Aligning pointer by using `_mm256_loadu_si256`
+            // on disaligned bytes.
             {
                 let align = VECTOR_SIZE - ($start_ptr as usize & VECTOR_ALIGN);
                 if align < VECTOR_SIZE {
@@ -97,15 +105,20 @@ macro_rules! loop_m256_128 {
                     if mask != 0 {
                         write_forward!(mask, align);
                     }
+                    // Aligning pointer
                     $ptr = $ptr.add(align);
                 }
             }
 
             debug_assert!($start_ptr <= $ptr && $start_ptr <= end_ptr.sub(VECTOR_SIZE));
 
+            // Process all aligned slices with at least one set of length `LOOP_SIZE`
             if LOOP_SIZE <= $len {
                 while $ptr <= end_ptr.sub(LOOP_SIZE) {
                     debug_assert_eq!(0, ($ptr as usize) % VECTOR_SIZE);
+
+                    // Using function `_mm256_load_si256` for faster behavior on aligened bytes.
+                    // Getting 4 sets of length `VECTOR_SIZE` each (`LOOP_SIZE=4*VECTOR_SIZE`)
                     let cmp_a = {
                         let a = _mm256_load_si256($ptr as *const __m256i);
                         masking!(a)
@@ -126,6 +139,10 @@ macro_rules! loop_m256_128 {
                         masking!(a)
                     };
 
+                    // Combining the 4 sets using `or` logic operator by pairs.
+                    // Then we write the mask for the 4 sets of `VECTOR_SIZE`
+                    // elements each, and make the pointer point to the next
+                    // `LOOP_SIZE` elements
                     if _mm256_movemask_epi8(_mm256_or_si256(
                         _mm256_or_si256(cmp_a, cmp_b),
                         _mm256_or_si256(cmp_c, cmp_d),
@@ -159,6 +176,9 @@ macro_rules! loop_m256_128 {
                 }
             }
 
+            // When the rest of string has a length greater then `VECTOR_SIZE`
+            // but less then `LOOP_SIZE`, we process it `VECTOR_SIZE` bits at
+            // a time until there are left less then `VECTOR_SIZE` elements
             while $ptr <= end_ptr.sub(VECTOR_SIZE) {
                 debug_assert_eq!(0, ($ptr as usize) % VECTOR_SIZE);
 
@@ -176,6 +196,8 @@ macro_rules! loop_m256_128 {
 
             debug_assert!(end_ptr.sub(VECTOR_SIZE) < $ptr);
 
+            // At this point at most there is less then `VECTOR_SIZE` elements
+            // so the macro `write_forward` is used to finalize de process
             if $ptr < end_ptr {
                 debug_assert_eq!(0, ($ptr as usize) % VECTOR_SIZE);
 
