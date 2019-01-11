a555521a0ef8df9fbc0ed2fc6f381ffc644f4bee
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-01-11 15:45:57 +0100

    Update comparative benches

--- benches/build.rs
+++ benches/build.rs
@@ -1,7 +1,18 @@
-use version_check::is_nightly;
+use version_check::{is_min_version, is_nightly};
 
 fn main() {
+    enable_nightly();
+    enable_simd_optimizations();
+}
+
+fn enable_nightly() {
     if is_nightly().unwrap_or(false) {
         println!("cargo:rustc-cfg=v_escape_benches_nightly");
+    }
+}
+
+fn enable_simd_optimizations() {
+    if !is_min_version("1.27.0").map_or(false, |(yes, _)| yes) {
+        println!("cargo:rustc-cfg=v_escape_nosimd");
     }
 }
