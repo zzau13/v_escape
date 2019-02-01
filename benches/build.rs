a900349d16825f9c3a0226447c8d7731b0e9ede7
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-02-01 23:46:44 +0100

    Add benches v_escape sub-attributes

--- benches/build.rs
+++ benches/build.rs
@@ -1,4 +1,5 @@
 use version_check::{is_min_version, is_nightly};
+use std::env;
 
 fn main() {
     enable_nightly();
@@ -12,7 +13,21 @@ fn enable_nightly() {
 }
 
 fn enable_simd_optimizations() {
-    if !is_min_version("1.27.0").map_or(false, |(yes, _)| yes) {
+    if is_env_set("CARGO_CFG_HTMLESCAPE_DISABLE_AUTO_SIMD")
+        || !is_min_version("1.27.0").map_or(false, |(yes, _)| yes)
+    {
         println!("cargo:rustc-cfg=v_escape_nosimd");
+        return;
     }
+
+    println!("cargo:rustc-cfg=v_simd");
+    println!("cargo:rustc-cfg=v_sse");
+
+    if !is_env_set("CARGO_CFG_HTMLESCAPE_DISABLE_AUTO_AVX") {
+        println!("cargo:rustc-cfg=v_avx");
+    }
+}
+
+fn is_env_set(name: &str) -> bool {
+    env::var(name).is_ok()
 }
