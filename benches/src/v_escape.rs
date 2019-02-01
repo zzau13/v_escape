9a1d7bf77b8229e2c9397c1b28ad804740b2d388
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-02-01 23:46:44 +0100

    Add benches v_escape sub-attributes

--- benches/src/v_escape.rs
+++ benches/src/v_escape.rs
@@ -1,11 +1,31 @@
 use criterion::Bencher;
 use std::fmt::Write;
 
-new_escape!(
-    MyEscape,
-    "#0->zero || #1->one || #2->two || #3->three || #4->four || #5->five || \
-     #6->six || #7->seven || #8->eight || #9->nine"
-);
+cfg_if! {
+    if #[cfg(all(v_simd, v_avx))] {
+        new_escape!(
+            MyEscape,
+            "#0->zero || #1->one || #2->two || #3->three || #4->four || #5->five || \
+             #6->six || #7->seven || #8->eight || #9->nine"
+        );
+    } else if #[cfg(all(v_simd, v_sse))] {
+        new_escape!(
+            MyEscape,
+            "#0->zero || #1->one || #2->two || #3->three || #4->four || #5->five || \
+             #6->six || #7->seven || #8->eight || #9->nine",
+             avx = false
+        );
+    } else {
+        new_escape!(
+            MyEscape,
+            "#0->zero || #1->one || #2->two || #3->three || #4->four || #5->five || \
+             #6->six || #7->seven || #8->eight || #9->nine",
+             simd = false
+        );
+    }
+}
+
+
 
 pub fn escaping(corpus: &'static [u8]) -> impl FnMut(&mut Bencher) + 'static {
     move |b: &mut Bencher| {
