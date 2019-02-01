71ab4d78ff14620835bd4147c7ac7571a4992a16
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-02-01 20:19:32 +0100

    Add benches at more case

--- benches/src/v_latexescape.rs
+++ benches/src/v_latexescape.rs
@@ -1,15 +1,10 @@
 use criterion::Bencher;
 use std::fmt::Write;
-
-new_escape!(
-    MyEscape,
-    "#0->zero || #1->one || #2->two || #3->three || #4->four || #5->five || \
-     #6->six || #7->seven || #8->eight || #9->nine"
-);
+use v_latexescape::LateXEscape as Escape;
 
 pub fn escaping(corpus: &'static [u8]) -> impl FnMut(&mut Bencher) + 'static {
     move |b: &mut Bencher| {
-        let e = MyEscape::new(corpus);
+        let e = Escape::new(corpus);
         let mut writer = String::new();
 
         b.iter(|| {
