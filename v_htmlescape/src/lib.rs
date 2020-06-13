868c23f1a76472997d3654eb7a894d65b0e784bd
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-06-13 18:24:22 +0000

    Reduce number of html escape characters

--- v_htmlescape/src/lib.rs
+++ v_htmlescape/src/lib.rs
@@ -7,6 +7,19 @@
 //! print!("{}", escape("foo<bar"));
 //! ```
 //!
+//! https://html.spec.whatwg.org/#serialising-html-fragments
+//! Escaping a string (for the purposes of the algorithm above) consists of running the following steps:
+//!
+//! Replace any occurrence of the "&" character by the string "&amp;".
+//!
+//! Replace any occurrences of the U+00A0 NO-BREAK SPACE character by the string "&nbsp;".
+//!
+//! If the algorithm was invoked in the attribute mode, replace any occurrences of the """
+//! character by the string "&quot;".
+//!
+//! If the algorithm was not invoked in the attribute mode, replace any occurrences of
+//! the "<" character by the string "&lt;", and any occurrences of the ">" character
+//! by the string "&gt;".
 #[macro_use]
 extern crate cfg_if;
 
@@ -17,7 +30,7 @@ extern crate v_escape;
 pub mod fallback {
     new_escape!(
         HTMLEscape,
-        "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot; || 39->&#x27; || 47->&#x2f;",
+        "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot;",
         simd = false
     );
 }
@@ -26,13 +39,13 @@ cfg_if! {
     if #[cfg(all(v_htmlescape_simd, v_htmlescape_avx))] {
         new_escape!(
             HTMLEscape,
-            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot; || 39->&#x27; || 47->&#x2f;",
+            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot;",
             simd = true, avx = true
         );
     } else if #[cfg(all(v_htmlescape_simd, v_htmlescape_sse))] {
         new_escape!(
             HTMLEscape,
-            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot; || 39->&#x27; || 47->&#x2f;",
+            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot;",
             simd = true, avx = false
         );
     } else {
@@ -56,8 +69,8 @@ mod test {
         assert_eq!(HTMLEscape::from("bar&h").to_string(), "bar&amp;h");
         assert_eq!(
             HTMLEscape::from("// my <html> is \"unsafe\" & should be 'escaped'").to_string(),
-            "&#x2f;&#x2f; my &lt;html&gt; is &quot;unsafe&quot; &amp; \
-             should be &#x27;escaped&#x27;"
+            "// my &lt;html&gt; is &quot;unsafe&quot; &amp; \
+             should be 'escaped'"
         );
         assert_eq!(
             HTMLEscape::from(
@@ -66,8 +79,8 @@ mod test {
                     .as_ref()
             )
             .to_string(),
-            "&#x2f;&#x2f; my &lt;html&gt; is &quot;unsafe&quot; &amp; \
-             should be &#x27;escaped&#x27;"
+            "// my &lt;html&gt; is &quot;unsafe&quot; &amp; \
+             should be 'escaped'"
                 .repeat(10_000)
         );
     }
--- v_htmlescape/tests/lib.rs
+++ v_htmlescape/tests/lib.rs
@@ -3,8 +3,8 @@ use v_htmlescape::HTMLEscape;
 #[test]
 fn test_escape() {
     let empty = "";
-    let escapes = "<>&\"'/";
-    let escaped = "&lt;&gt;&amp;&quot;&#x27;&#x2f;";
+    let escapes = "<>&\"";
+    let escaped = "&lt;&gt;&amp;&quot;";
     let string_long: &str = &"foobar".repeat(1024);
 
     // https://gitlab.com/r-iendo/v_escape/issues/2
@@ -22,8 +22,8 @@ fn test_escape() {
     assert_eq!(HTMLEscape::from("bar&h").to_string(), "bar&amp;h");
     assert_eq!(
         HTMLEscape::from("// my <html> is \"unsafe\" & should be 'escaped'").to_string(),
-        "&#x2f;&#x2f; my &lt;html&gt; is &quot;unsafe&quot; &amp; \
-         should be &#x27;escaped&#x27;"
+        "// my &lt;html&gt; is &quot;unsafe&quot; &amp; \
+         should be 'escaped'"
     );
     assert_eq!(
         HTMLEscape::from("<".repeat(16).as_ref()).to_string(),
@@ -94,48 +94,9 @@ fn test_escape() {
         [string_long, "&lt;", string_long, escaped, string_long,].join("")
     );
 
-    let string_long = r#"
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat tellus sit
-    amet ornare fermentum. Etiam nec erat ante. In at metus a orci mollis scelerisque.
-    Sed eget ultrices turpis, at sollicitudin erat. Integer hendrerit nec magna quis
-    venenatis. Vivamus non dolor hendrerit, vulputate velit sed, varius nunc. Quisque
-    in pharetra mi. Sed ullamcorper nibh malesuada commodo porttitor. Ut scelerisque
-    sodales felis quis dignissim. Morbi aliquam finibus justo, sit amet consectetur
-    mauris efficitur sit amet. Donec posuere turpis felis, eu lacinia magna accumsan
-    quis. Fusce egestas lacus vel fermentum tincidunt. Phasellus a nulla eget lectus
-    placerat commodo at eget nisl. Fusce cursus dui quis purus accumsan auctor.
-    Donec iaculis felis quis metus consectetur porttitor.
-<p>
-    Etiam nibh mi, <b>accumsan</b> quis purus sed, posuere fermentum lorem. In pulvinar porta
-    maximus. Fusce tincidunt lacinia tellus sit amet tincidunt. Aliquam lacus est, pulvinar
-    non metus a, <b>facilisis</b> ultrices quam. Nulla feugiat leo in cursus eleifend. Suspendisse
-    eget nisi ac justo sagittis interdum id a ipsum. Nulla mauris justo, scelerisque ac
-    rutrum vitae, consequat vel ex.
-</p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p></p>
-<p>
-    Sed sollicitudin <b>sem</b> mauris, at rutrum nibh egestas vel. Ut eu nisi tellus. Praesent dignissim
-    orci elementum, mattis turpis eget, maximus ante. Suspendisse luctus eu felis a tempor. Morbi
-    ac risus vitae sem molestie ullamcorper. Curabitur ligula augue, sollicitudin quis maximus vel,
-    facilisis sed nibh. Aenean auctor magna sem, id rutrum metus convallis quis. Nullam non arcu
-    dictum, lobortis erat quis, rhoncus est. Suspendisse venenatis, mi sed venenatis vehicula,
-    tortor dolor egestas lectus, et efficitur turpis odio non augue. Integer velit sapien, dictum
-    non egestas vitae, hendrerit sed quam. Phasellus a nunc eu erat varius imperdiet. Etiam id
-    sollicitudin turpis, vitae molestie orci. Quisque ornare magna quis metus rhoncus commodo.
-    Phasellus non mauris velit.
-</p>
-<p>
-    Etiam dictum tellus ipsum, nec varius quam ornare vel. Cras vehicula diam nec sollicitudin
-    ultricies. Pellentesque rhoncus sagittis nisl id facilisis. Nunc viverra convallis risus ut
-    luctus. Aliquam vestibulum <b>efficitur massa</b>, id tempus nisi posuere a. Aliquam scelerisque
-    elit justo. Nullam a ante felis. Cras vitae lorem eu nisi feugiat hendrerit. Maecenas vitae
-    suscipit leo, lacinia dignissim lacus. Sed eget volutpat mi. In eu bibendum neque. Pellentesque
-    finibus velit a fermentum rhoncus. Maecenas leo purus, eleifend eu lacus a, condimentum sagittis
-    justo.
-</p>"#;
-    let string_long_escaped = "\n    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat tellus sit\n    amet ornare fermentum. Etiam nec erat ante. In at metus a orci mollis scelerisque.\n    Sed eget ultrices turpis, at sollicitudin erat. Integer hendrerit nec magna quis\n    venenatis. Vivamus non dolor hendrerit, vulputate velit sed, varius nunc. Quisque\n    in pharetra mi. Sed ullamcorper nibh malesuada commodo porttitor. Ut scelerisque\n    sodales felis quis dignissim. Morbi aliquam finibus justo, sit amet consectetur\n    mauris efficitur sit amet. Donec posuere turpis felis, eu lacinia magna accumsan\n    quis. Fusce egestas lacus vel fermentum tincidunt. Phasellus a nulla eget lectus\n    placerat commodo at eget nisl. Fusce cursus dui quis purus accumsan auctor.\n    Donec iaculis felis quis metus consectetur porttitor.\n&lt;p&gt;\n    Etiam nibh mi, &lt;b&gt;accumsan&lt;&#x2f;b&gt; quis purus sed, posuere fermentum lorem. In pulvinar porta\n    maximus. Fusce tincidunt lacinia tellus sit amet tincidunt. Aliquam lacus est, pulvinar\n    non metus a, &lt;b&gt;facilisis&lt;&#x2f;b&gt; ultrices quam. Nulla feugiat leo in cursus eleifend. Suspendisse\n    eget nisi ac justo sagittis interdum id a ipsum. Nulla mauris justo, scelerisque ac\n    rutrum vitae, consequat vel ex.\n&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;&lt;&#x2f;p&gt;\n&lt;p&gt;\n    Sed sollicitudin &lt;b&gt;sem&lt;&#x2f;b&gt; mauris, at rutrum nibh egestas vel. Ut eu nisi tellus. Praesent dignissim\n    orci elementum, mattis turpis eget, maximus ante. Suspendisse luctus eu felis a tempor. Morbi\n    ac risus vitae sem molestie ullamcorper. Curabitur ligula augue, sollicitudin quis maximus vel,\n    facilisis sed nibh. Aenean auctor magna sem, id rutrum metus convallis quis. Nullam non arcu\n    dictum, lobortis erat quis, rhoncus est. Suspendisse venenatis, mi sed venenatis vehicula,\n    tortor dolor egestas lectus, et efficitur turpis odio non augue. Integer velit sapien, dictum\n    non egestas vitae, hendrerit sed quam. Phasellus a nunc eu erat varius imperdiet. Etiam id\n    sollicitudin turpis, vitae molestie orci. Quisque ornare magna quis metus rhoncus commodo.\n    Phasellus non mauris velit.\n&lt;&#x2f;p&gt;\n&lt;p&gt;\n    Etiam dictum tellus ipsum, nec varius quam ornare vel. Cras vehicula diam nec sollicitudin\n    ultricies. Pellentesque rhoncus sagittis nisl id facilisis. Nunc viverra convallis risus ut\n    luctus. Aliquam vestibulum &lt;b&gt;efficitur massa&lt;&#x2f;b&gt;, id tempus nisi posuere a. Aliquam scelerisque\n    elit justo. Nullam a ante felis. Cras vitae lorem eu nisi feugiat hendrerit. Maecenas vitae\n    suscipit leo, lacinia dignissim lacus. Sed eget volutpat mi. In eu bibendum neque. Pellentesque\n    finibus velit a fermentum rhoncus. Maecenas leo purus, eleifend eu lacus a, condimentum sagittis\n    justo.\n&lt;&#x2f;p&gt;";
     let string_short = "Lorem ipsum dolor sit amet,<foo>bar&foo\"bar\\foo/bar";
     let string_short_escaped =
-        "Lorem ipsum dolor sit amet,&lt;foo&gt;bar&amp;foo&quot;bar\\foo&#x2f;bar";
+        "Lorem ipsum dolor sit amet,&lt;foo&gt;bar&amp;foo&quot;bar\\foo/bar";
     let no_escape = "Lorem ipsum dolor sit amet,";
     let no_escape_long = r#"
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque eu urna in aliquet.
@@ -160,9 +121,5 @@ quis lacus at, gravida maximus elit. Duis tristique, nisl nullam.
     assert_eq!(
         HTMLEscape::from(string_short.repeat(1024).as_ref()).to_string(),
         string_short_escaped.repeat(1024)
-    );
-    assert_eq!(
-        HTMLEscape::from(string_long).to_string(),
-        string_long_escaped
     );
 }
