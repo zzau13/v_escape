857e0c8f1fbc020fd3b45373560b679c63926c24
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-06-15 17:55:11 +0000

    Revert less html escape characters

--- v_htmlescape/src/lib.rs
+++ v_htmlescape/src/lib.rs
@@ -7,19 +7,6 @@
 //! print!("{}", escape("foo<bar"));
 //! ```
 //!
-//! https://html.spec.whatwg.org/#serialising-html-fragments
-//! Escaping a string (for the purposes of the algorithm above) consists of running the following steps:
-//!
-//! Replace any occurrence of the "&" character by the string "&amp;".
-//!
-//! Replace any occurrences of the U+00A0 NO-BREAK SPACE character by the string "&nbsp;".
-//!
-//! If the algorithm was invoked in the attribute mode, replace any occurrences of the """
-//! character by the string "&quot;".
-//!
-//! If the algorithm was not invoked in the attribute mode, replace any occurrences of
-//! the "<" character by the string "&lt;", and any occurrences of the ">" character
-//! by the string "&gt;".
 #[macro_use]
 extern crate cfg_if;
 
@@ -30,7 +17,7 @@ extern crate v_escape;
 pub mod fallback {
     new_escape!(
         HTMLEscape,
-        "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot;",
+        "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot; || 39->&#x27; || 47->&#x2f;",
         simd = false
     );
 }
@@ -39,13 +26,13 @@ cfg_if! {
     if #[cfg(all(v_htmlescape_simd, v_htmlescape_avx))] {
         new_escape!(
             HTMLEscape,
-            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot;",
+            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot; || 39->&#x27; || 47->&#x2f;",
             simd = true, avx = true
         );
     } else if #[cfg(all(v_htmlescape_simd, v_htmlescape_sse))] {
         new_escape!(
             HTMLEscape,
-            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot;",
+            "60->&lt; || 62->&gt; || 38->&amp; || 34->&quot; || 39->&#x27; || 47->&#x2f;",
             simd = true, avx = false
         );
     } else {
@@ -69,8 +56,8 @@ mod test {
         assert_eq!(HTMLEscape::from("bar&h").to_string(), "bar&amp;h");
         assert_eq!(
             HTMLEscape::from("// my <html> is \"unsafe\" & should be 'escaped'").to_string(),
-            "// my &lt;html&gt; is &quot;unsafe&quot; &amp; \
-             should be 'escaped'"
+            "&#x2f;&#x2f; my &lt;html&gt; is &quot;unsafe&quot; &amp; \
+             should be &#x27;escaped&#x27;"
         );
         assert_eq!(
             HTMLEscape::from(
@@ -79,8 +66,8 @@ mod test {
                     .as_ref()
             )
             .to_string(),
-            "// my &lt;html&gt; is &quot;unsafe&quot; &amp; \
-             should be 'escaped'"
+            "&#x2f;&#x2f; my &lt;html&gt; is &quot;unsafe&quot; &amp; \
+             should be &#x27;escaped&#x27;"
                 .repeat(10_000)
         );
     }
