b0f839c9fe807690a66a34f0878d5b7b3a9cca75
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-07-18 14:43:05 +0000

    Fix use is_x86_feature_detected out x86 arch

--- v_escape/build.rs
+++ v_escape/build.rs
@@ -3,11 +3,14 @@ fn main() {
 }
 
 fn enable_simd_optimizations() {
-    if is_x86_feature_detected!("sse2") {
-        println!("cargo:rustc-cfg=v_escape_sse");
-    }
+    #[cfg(target_arch = "x86_64")]
+    {
+        if is_x86_feature_detected!("sse2") {
+            println!("cargo:rustc-cfg=v_escape_sse");
+        }
 
-    if is_x86_feature_detected!("avx2") {
-        println!("cargo:rustc-cfg=v_escape_avx");
+        if is_x86_feature_detected!("avx2") {
+            println!("cargo:rustc-cfg=v_escape_avx");
+        }
     }
 }
