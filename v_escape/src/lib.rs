3a5b296e992d43c673eb2128016219f922082a63
Juan Aguilar Santillana <mhpoin@gmail.com>
2021-10-12 02:00:54 +0200

    Add doctest on README and fix issues

--- v_escape/src/lib.rs
+++ v_escape/src/lib.rs
@@ -428,3 +428,15 @@ macro_rules! cfg_escape_bytes {
         }
     }};
 }
+
+#[cfg(doctest)]
+mod test_readme {
+    macro_rules! external_doc_test {
+        ($x:expr) => {
+            #[doc = $x]
+            extern "C" {}
+        };
+    }
+
+    external_doc_test!(include_str!("../../README.md"));
+}
