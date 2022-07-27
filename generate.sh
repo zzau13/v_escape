b3a9489d274a3adb9d6eaa60559fb89fc7b190ed
Juan Aguilar Santillana <mhpoin@gmail.com>
2022-07-27 18:50:53 +0200

    chore: add generate script

---
+++ generate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+cargo install --path codegen
+v_escape_codegen -i v_latexescape
+v_escape_codegen -i v_jsonescape
+v_escape_codegen -i v_htmlescape
+
+cargo fmt
+cargo test --all-features
